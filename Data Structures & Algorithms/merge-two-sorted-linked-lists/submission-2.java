cdd3e5c8954c7bdfdfc2a619d83e0f9dc4a726b3
Gavin Honey <109183314+ghoney47@users.noreply.github.com>
2026-06-08 10:38:37 -0700

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null)
+            return null;
+
+
+        ListNode curr1 = list1;
+        ListNode curr2 = list2;
+
+        ListNode output = new ListNode(-5);
+        ListNode currOut = output;
+
+       
+
+
+        while (curr1 != null || curr2 != null){
+            //System.out.println("curr1: " + curr1.val + ", curr2: " + curr2.val);
+            //null checking
+            if (curr1 == null){
+                currOut.next = curr2;
+                break;
+            } else if (curr2 == null){
+                currOut.next = curr1;
+                break;
+            //value comparison
+            } else if(curr1.val <= curr2.val){
+                currOut.next = curr1;
+                if (curr1 != null){
+                    curr1 = curr1.next;
+                } else{
+                    curr1 = null;
+                }
+            } else {
+                currOut.next = curr2;
+
+                if (curr2 != null){
+                    curr2 = curr2.next;
+                } else{
+                    curr2 = null;
+                }
+            }
+
+
+            //updating currOut pointer
+            currOut = currOut.next;
+        }
+
+        output = output.next;
+
+        return output;
+    }
+}
