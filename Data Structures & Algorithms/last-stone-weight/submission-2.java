4027b8a62dbfff215556eb37a918c6f5aa3d2374
Gavin Honey <109183314+ghoney47@users.noreply.github.com>
2026-06-17 07:59:14 -0700

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        ArrayList<Integer> st = new ArrayList<>();
+        
+        for (int i : stones){
+            st.add(i);
+        }
+
+        while (st.size() > 1){
+            Collections.sort(st, Collections.reverseOrder());
+            int x = st.get(0);
+            int y = st.get(1);
+
+            if (x == y){
+                st.remove(0);
+                st.remove(0);
+            }
+
+            if (x > y){
+                //removing y
+                st.remove(1);
+
+                //update x's value
+                x = x - y;
+                st.add(0, x);
+
+                //remove old x value
+                st.remove(1);
+            }
+
+            System.out.println(st.toString());
+        }
+
+        if (st.size() == 0){
+            return 0;
+        }
+
+        return st.get(0);
+    }
+}
